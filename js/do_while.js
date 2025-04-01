1655466a6919fcc9997791260e43aa80cb87c595
balerasabrina <sabrinabalera.dev2024@gmail.com>
2025-04-01 11:15:28 -0300

    exercicio 30

---
+++ js/do_while.js
@@ -0,0 +1,12 @@
+let resultadoDiv = document.getElementById("resultado");
+let numeros = "";
+let i = 1;
+
+do{
+    if (i%5 ==0){
+        numeros += i + "<br>"
+    }
+    i++;
+}while(i<=100);
+
+resultadoDiv.innerHTML = numeros;
